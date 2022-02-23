7ffad3f9c78c9716cb84ac7fe81d8d47e350a117
alaqsaka <aqsaabidin15@gmail.com>
2022-02-23 16:15:32 +0700

    feat: search features done function

---
+++ db/functions.php
@@ -0,0 +1,24 @@
+<?php 
+// koneksi ke database
+    $conn = mysqli_connect("localhost", "root", "", "inventory_db");
+    
+    function query($query){
+        global $conn;
+        $result = mysqli_query($conn, $query);
+
+        $rows = [];
+        while($row = mysqli_fetch_assoc($result)) {
+            $rows[] = $row;
+        }
+
+        return $rows;
+    }
+    
+    
+    function search($keyword){
+        $query = "SELECT * from inventory 
+                WHERE itemsname = '$keyword'";
+
+        return query($query);
+    }
+?>
--- viewitems.php
+++ viewitems.php
@@ -3,9 +3,23 @@
 
     require_once 'includes/header.php';
     require_once 'db/conn.php';
+    require 'db/functions.php';
 
     $results = $crud->getItems();
+
+    if(isset($_POST["search"])) {
+        $results = search($_POST["keyword"]);
+    } else {
+        
+    }
 ?>
+    <!-- Search feature -->
+    <form action="" method="post" class="mb-2">
+        
+        <input type="text" name="keyword" class="form-control" autofocus autocomplete="off"placeholder="Search Items Here">
+        <button type="submit" name="search" class="btn btn-primary">Search</button>
+    </form>
+
     <table class="table table-light table-bordered" style="border: 1px solid #E5E5E5 !important;">
         <tr>
             <th colspan="7" class="title-head">Items Registred</th>
@@ -28,20 +42,45 @@
             <td>0218412124213</td>
             <td>2/15/20022</td>
         </tr> -->
-        <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
-            <tr>
+        <?php 
+            if(isset($_POST["search"])) {
+            ?>
+            <?php $i =1; ?>
+            <?php foreach($results as $r) :?>
+                <tr>
                 <td><?php echo $r['item_id'] ?></td>
-                <td><?php echo $r['itemsname'] ?></td>
-                <td><?php echo $r['price'] ?></td>
-                <td><?php echo $r['quantity'] ?></td>
-                <td><?php echo $r['dateoftransaction'] ?></td>
-                <td>
-                    <a class="btn btn-primary" href="view.php?id=<?php echo $r['item_id'] ?>">View Details</a>
-                    <a class="btn btn-warning" href="edit.php?id=<?php echo $r['item_id'] ?>">Edit Details</a>
-                    <a onclick="return confirm('Are u sure u want to delete this items?');" class="btn btn-danger" href="delete.php?id=<?php echo $r['item_id'] ?>">Delete</a>
-                </td>
-            </tr>
+                    <td><?php echo $r['itemsname'] ?></td>
+                    <td><?php echo $r['price'] ?></td>
+                    <td><?php echo $r['quantity'] ?></td>
+                    <td><?php echo $r['dateoftransaction'] ?></td>
+                    <td>
+                        <a class="btn btn-primary" href="view.php?id=<?php echo $r['item_id'] ?>">View Details</a>
+                        <a class="btn btn-warning" href="edit.php?id=<?php echo $r['item_id'] ?>">Edit Details</a>
+                        <a onclick="return confirm('Are u sure u want to delete this items?');" class="btn btn-danger" href="delete.php?id=<?php echo $r['item_id'] ?>">Delete</a>
+                    </td>
+                </tr>
+            <?php $i++;?>
+            <?php endforeach;?>
+        <?php } else {
+        ?>
+            <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
+                <tr>
+                    <td><?php echo $r['item_id'] ?></td>
+                    <td><?php echo $r['itemsname'] ?></td>
+                    <td><?php echo $r['price'] ?></td>
+                    <td><?php echo $r['quantity'] ?></td>
+                    <td><?php echo $r['dateoftransaction'] ?></td>
+                    <td>
+                        <a class="btn btn-primary" href="view.php?id=<?php echo $r['item_id'] ?>">View Details</a>
+                        <a class="btn btn-warning" href="edit.php?id=<?php echo $r['item_id'] ?>">Edit Details</a>
+                        <a onclick="return confirm('Are u sure u want to delete this items?');" class="btn btn-danger" href="delete.php?id=<?php echo $r['item_id'] ?>">Delete</a>
+                    </td>
+                </tr>
+            <?php }?>
+            
         <?php }?>
+        
+        
     </table>
 
 <?php require_once 'includes/footer.php'; ?>
