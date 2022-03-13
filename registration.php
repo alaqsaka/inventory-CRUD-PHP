8ab59c1b047481162c210af3ac2c5bd09762105c
alaqsaka <aqsaabidin15@gmail.com>
2022-03-13 16:39:25 +0700

    feat: adding style register page

--- registration.php
+++ registration.php
@@ -1,3 +1,36 @@
+<html>
+    <style>
+        .titles { 
+            font-family: Poppins;
+            font-size: 36px;
+            font-style: normal;
+            font-weight: 600;
+            line-height: 54px;
+            letter-spacing: 0em;
+            text-align: left;
+            color: #069120;
+
+        }
+
+        .register-form > input{
+            width: 487px;
+            border: none;
+            border-radius: 4px;
+            height: 45px;
+        }
+
+        .register-form label {
+            text-align: left;
+        }
+
+        .btn-green { 
+            background: #069120 !important;
+            width: 100%;
+            color: #fff !important;
+        }
+    </style>
+</html>
+
 <?php
     $title = 'Account Registrations'; 
 
@@ -15,28 +48,27 @@
         }
     }
 ?>
-        <h1>Registration Page</h1>
-        <form action="" method="post">
-            <ul>
-                <li>
-                    <label for="username">Username: </label><br>
-                    <input type="text" name="username" id="username">
-                </li>
-                <li>
-                    <label for="password">Password: </label><br>
-                    <input type="password" name="password" id="password">
-                </li>
-                <li>
-                    <label for="confirmPassword">Confirmation Password </label><br>
-                    <input type="password" name="confirmPassword" id="confirmPassword">
-                </li>
-                <li>
-                    <br>
-                    <button type="submit" name="register">Create new account</button>
-                </li>
-            </ul>
-        </form>
-        <br>
+
+<div style="max-width: 487px; margin: 0 auto;">
+    <h1 class="titles">Create Your Free Account</h1>
+    <form action="" method="post">            
+        <div class="form-group register-form">
+            <label for="username">Username: </label><br>
+            <input type="text" name="username" id="username">
+        </div>
+        <div class="form-group register-form">
+            <label for="password">Password: </label><br>
+            <input type="password" name="password" id="password">
+        </div>
+        <div class="form-group register-form">
+            <label for="confirmPassword">Confirmation Password </label><br>
+            <input type="password" name="confirmPassword" id="confirmPassword">
+        </div>
+        
+        <button type="submit" name="register" class="btn btn-green">Create new account</button>
+    </form>
+</div>
+       
 <br>
 <br>
 <br>
